322cd7ab76bb8519560c2561ad23f6d598eedd6b
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2018-07-20 12:04:52 -0300

    Sets version 1.0.0

--- metroclima/__version__.py
+++ metroclima/__version__.py
@@ -8,7 +8,7 @@ __title__ = 'metroclima'
 __description__ = 'A simple Python tool to retrieve information from ' \
                   'the Porto Alegre city\'s Metroclima database.'
 __url__ = 'https://github.com/jonathadv/metroclima-dump'
-__version__ = '0.2.0'
+__version__ = '1.0.0'
 __author__ = 'Jonatha Daguerre Vasconcelos'
 __author_email__ = 'jonatha@daguerre.com.br'
 __license__ = 'MIT'
